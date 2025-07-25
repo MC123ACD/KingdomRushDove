797fe84c6f0582693cf5050caad0578ddcf066e6
CrazySpottedDove <3230104178@zju.edu.cn>
2025-07-25 14:57:55 +0800

    调整沙王连斩冷却

--- kr1/heroes.lua
+++ kr1/heroes.lua
@@ -2248,7 +2248,7 @@ local function heroes()
     tt.hero.level_stats.melee_damage_max = {12, 14, 16, 18, 20, 22, 24, 26, 28, 30}
     tt.hero.level_stats.melee_damage_min = {7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
     tt.hero.skills.flurry = E:clone_c("hero_skill")
-    tt.hero.skills.flurry.cooldown = {6, 5.5, 5}
+    tt.hero.skills.flurry.cooldown = {6, 6, 6}
     tt.hero.skills.flurry.loops = {2, 3, 4}
     tt.hero.skills.flurry.xp_level_steps = {
         [2] = 1,
